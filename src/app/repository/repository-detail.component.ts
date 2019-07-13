bd4e03038f3281e0558cd9f1075445321a5066fb
markfullmer <mfullmer@gmail.com>
2019-07-13 16:34:19 -0500

    Support highlighting of repository plaintext

--- src/app/repository/repository-detail.component.ts
+++ src/app/repository/repository-detail.component.ts
@@ -4,6 +4,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { APIService } from '../services/api.service';
 import { RepositoryDetail } from '../repository/repository-detail';
 import { RepositoryHelper } from '../repository/repository-helper';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   templateUrl: '../repository/repository-detail.component.html',
@@ -25,48 +27,54 @@ export class RepositoryDetailComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((routeParams) => {
-      this.API.getRepositoryDetailById(routeParams.id).subscribe(response => {
-        if (response && response[0]) {
-          this.content = response[0];
-          this.content.label = this.repositoryHelper.getLabel(this.content.document_type, this.content.course, this.content.assignment);
-          this.content.embed_uri = this.sanitizer.bypassSecurityTrustResourceUrl( "http://docs.google.com/gview?url=" + this.content.uri + "&embedded=true");
-          this.isLoaded = true;
-          let relatedTexts = {
-            'course': this.content.course,
-            'assignment': this.content.assignment,
-            'institution': this.content.institution
-          };
-          if (this.content.assignment == '') {
+    combineLatest(this.route.params, this.route.queryParams)
+      .pipe(map(routes => ({ params: routes[0], query: routes[1] })))
+      .subscribe(routes => {
+        let repositoryRequest = {
+          'id': routes.params.id,
+          'search': routes.query.search
+        };
+        this.API.getRepositoryReferenceByMetadata(repositoryRequest).subscribe(response => {
+          if (response && response[0]) {
+            this.content = response[0];
+            this.content.label = this.repositoryHelper.getLabel(this.content.document_type, this.content.course, this.content.assignment);
+            this.content.embed_uri = this.sanitizer.bypassSecurityTrustResourceUrl("http://docs.google.com/gview?url=" + this.content.uri + "&embedded=true");
+            this.isLoaded = true;
             let relatedTexts = {
               'course': this.content.course,
-              'year': this.content.year,
-              'semester': this.content.semester,
+              'assignment': this.content.assignment,
               'institution': this.content.institution
             };
+            if (this.content.assignment == '') {
+              let relatedTexts = {
+                'course': this.content.course,
+                'year': this.content.year,
+                'semester': this.content.semester,
+                'institution': this.content.institution
+              };
+            }
+            // Retrieve all texts with similar metadata
+            this.API.getCorpusReferenceByMetadata(relatedTexts).subscribe(response => {
+              if (response && response != '') {
+                this.relatedTexts = response;
+              }
+            });
+            let repositoryParameters = {
+              'course': this.content.course,
+              'assignment': this.content.assignment,
+              'institution': this.content.institution
+            };
+            this.API.getRepositoryReferenceByMetadata(repositoryParameters).subscribe(response => {
+              if (response && response != '') {
+                this.relatedResources = response;
+              }
+            });
           }
-          // Retrieve all texts with similar metadata
-          this.API.getCorpusReferenceByMetadata(relatedTexts).subscribe(response => {
-            if (response && response != '') {
-              this.relatedTexts = response;
-            }
-          });
-          let repositoryParameters = {
-            'course': this.content.course,
-            'assignment': this.content.assignment,
-            'institution': this.content.institution
-          };
-          this.API.getRepositoryReferenceByMetadata(repositoryParameters).subscribe(response => {
-            if (response && response != '') {
-              this.relatedResources = response;
-            }
-          }); 
-        }
-        else {
-          this.router.navigateByUrl('404', { skipLocationChange: true });
-        }
+          else {
+            this.router.navigateByUrl('404', { skipLocationChange: true });
+          }
+        });
       });
-    });
   }
 
 }
