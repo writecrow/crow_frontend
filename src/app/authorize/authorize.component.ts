1f365d44276c702444b7f71bbdbf621eb44bd9e5
markfullmer <mfullmer@gmail.com>
2019-12-28 12:13:37 -0600

    Initial syntax cleanup

--- src/app/authorize/authorize.component.ts
+++ src/app/authorize/authorize.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { authorizeService } from '../services/authorize.service';
 import { LoginService} from '../services/login.service';
@@ -10,19 +10,19 @@ import { APIService } from '../services/api.service';
   providers: [authorizeService],
 })
 
-export class AuthorizeComponent {
+export class AuthorizeComponent implements OnInit {
 
   constructor(
     private router: Router,
-    public authorizeService: authorizeService,
-    private api : APIService,
-    public LoginService: LoginService,
+    public auth: authorizeService,
+    private api: APIService,
+    public login: LoginService,
     private globals: Globals
   ) { }
 
   ngOnInit(): void {
     // There is no reason for currently authenticated users to see this route.
-    if (this.authorizeService.isAuthenticated()) {
+    if (this.auth.isAuthenticated()) {
       this.router.navigate(['/']);
     }
   }
@@ -31,13 +31,13 @@ export class AuthorizeComponent {
     this.globals.authenticating = true;
     name = name.trim();
     if (!name || additional !== "" ) { return; }
-    let user: any = { name, pass };
-    this.LoginService.login(user.name, user.pass).subscribe(
+    const user: any = { name, pass };
+    this.login.login(user.name, user.pass).subscribe(
       data => {
         // 'data' is the Token object
         this.router.navigate(['/']);
         this.globals.isAuthenticated = true;
-        this.globals.authenticating = false; 
+        this.globals.authenticating = false;
         this.api.getDefaultCorpusSearchResults().subscribe(response => {
         });
       },
