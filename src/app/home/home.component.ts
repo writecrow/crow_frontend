ab95de38f6633dc7f90e3b8acccca9f69db7ff23
Mark Fullmer <mfullmer@gmail.com>
2020-05-28 15:58:43 -0700

    Add ability to display dynamic homepage content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,10 @@ import { AuthorizeComponent } from '../authorize/authorize.component';
 export class HomeComponent implements OnInit {
   total_words: number;
   total_texts: number;
+  homeFirstTitle: '';
+  homeFirstBody: '';
+  homeSecondTitle: '';
+  homeSecondBody: '';
 
   constructor(
     private API: APIService,
@@ -23,8 +27,24 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.total_words = 7283469;
     this.total_texts = 8254;
+    this.route.params.subscribe(() => {
+      this.API.getPage('home-first').subscribe(response => {
+        if (response && response[0]) {
+          this.homeFirstTitle = response[0].title;
+          this.homeFirstBody = response[0].body;
+        }
+      });
+    });
+    this.route.params.subscribe(() => {
+      this.API.getPage('home-second').subscribe(response => {
+        if (response && response[0]) {
+          this.homeSecondTitle = response[0].title;
+          this.homeSecondBody = response[0].body;
+        }
+      });
+    });
     if (typeof this.route.snapshot.queryParams.auth !== 'undefined') {
-      const auth = this.route.snapshot.queryParams.auth.split(":");
+      const auth = this.route.snapshot.queryParams.auth.split(':');
       if (auth[1]) {
         this.authComponent.authorize(auth[0].toLowerCase(), auth[1], '');
       }
