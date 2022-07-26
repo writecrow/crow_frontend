5b598200ed24b11e1c449c3b698bdff2715109af
Mark Fullmer <mfullmer@gmail.com>
2022-07-26 10:45:18 -0700

    Add progress to download

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { APIService } from '../services/api.service';
 import { environment } from '../../environments/environment';
+import { Globals } from '../globals';
 
 @Component({
   templateUrl: '../download/download.component.html',
@@ -20,10 +21,12 @@ export class DownloadComponent implements OnInit {
     public authorizeService: authorizeService,
     private router: Router,
     private API: APIService,
+    public globals: Globals,
   ) {
   }
 
   ngOnInit() {
+    this.globals.inProgress = false;
     this.href = this.router.url;
     this.API.getRoles().subscribe(response => {
       if (response) {
@@ -35,11 +38,12 @@ export class DownloadComponent implements OnInit {
   }
 
   offlineDownload() {
+    this.globals.inProgress = true;
     this.API.offlineCorpus().subscribe(response => {
       if (response) {
         // Based on https://fullstacktips.blogspot.com/2018/06/generate-downloadable-csv-file-from.html
         const data = response;
-        const filename = "crow-export.zip";
+        const filename = "crow-offline-v1.zip";
 
         const blob = data.constructor !== Blob
           ? new Blob([data], { type: 'text/zip' || 'application/octet-stream' })
@@ -60,6 +64,7 @@ export class DownloadComponent implements OnInit {
         lnk.href = objectURL = url.createObjectURL(blob);
         lnk.dispatchEvent(new MouseEvent('click'));
         setTimeout(url.revokeObjectURL.bind(url, objectURL));
+        this.globals.inProgress = false;
         return;
       }
     });
